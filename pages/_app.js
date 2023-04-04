e3bde666574c152b44584d5a13140b5158d549d5
mpotech-segovia <carlosegovia_mktg@mpotech.net>
2023-04-04 17:49:55 +0800

    step form components

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import "@fontsource/ubuntu/700.css";
 import "@/styles/layout/index.scss";
 import "@/styles/helpers/variables.scss";
 import "@/styles/pages/home.scss";
+import "@/styles/components/step-form.scss";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- pages/index.js
+++ pages/index.js
@@ -1,89 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
+
+import StepForm from "@/components/step-form";
 
 export default function Home() {
-  const [step, showStep] = useState("step-1");
-
-  function handleOnChange(event) {
-    let value = event.target.value;
-    showStep(value);
-    console.log(value);
-  }
-
   return (
     <main className="multi-step">
       <div className="multi-step__form">
-        <div className="step-wrapper">
-          <form className="radio-form">
-            <div className="input-group">
-              <input
-                type="radio"
-                id="step-1"
-                name="radio-step"
-                value="step-1"
-                checked={step === "step-1"}
-                onChange={handleOnChange}
-              />
-              <div className="step-info">
-                <div className="info-number">1</div>
-                <div className="info-context">
-                  <div className="context-step">Step 1</div>
-                  <div className="context-title">Your Info</div>
-                </div>
-              </div>
-            </div>
-            <div className="input-group">
-              <input
-                type="radio"
-                id="step-2"
-                name="radio-step"
-                value="step-2"
-                checked={step === "step-2"}
-                onChange={handleOnChange}
-              />
-              <div className="step-info">
-                <div className="info-number">2</div>
-                <div className="info-context">
-                  <div className="context-step">Step 2</div>
-                  <div className="context-title">Select Plan</div>
-                </div>
-              </div>
-            </div>
-            <div className="input-group">
-              <input
-                type="radio"
-                id="step-3"
-                name="radio-step"
-                value="step-3"
-                checked={step === "step-3"}
-                onChange={handleOnChange}
-              />
-              <div className="step-info">
-                <div className="info-number">3</div>
-                <div className="info-context">
-                  <div className="context-step">Step 3</div>
-                  <div className="context-title">Add-ons</div>
-                </div>
-              </div>
-            </div>
-            <div className="input-group">
-              <input
-                type="radio"
-                id="step-4"
-                name="radio-step"
-                value="step-4"
-                checked={step === "step-4"}
-                onChange={handleOnChange}
-              />
-              <div className="step-info">
-                <div className="info-number">4</div>
-                <div className="info-context">
-                  <div className="context-step">Step 4</div>
-                  <div className="context-title">Summary</div>
-                </div>
-              </div>
-            </div>
-          </form>
-        </div>
+        <StepForm></StepForm>
         <div className="info-container"></div>
       </div>
     </main>
