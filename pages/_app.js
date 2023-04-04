d340c734e6b87d52c10ae694eadd2d8e6828f061
mpotech-segovia <carlosegovia_mktg@mpotech.net>
2023-04-04 17:35:53 +0800

    first commit and initial design

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
-import '@/styles/globals.css'
+import "@fontsource/ubuntu/400.css";
+import "@fontsource/ubuntu/500.css";
+import "@fontsource/ubuntu/700.css";
+import "@/styles/layout/index.scss";
+import "@/styles/helpers/variables.scss";
+import "@/styles/pages/home.scss";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
