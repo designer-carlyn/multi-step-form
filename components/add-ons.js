41bef8a7c7c07b676ca319f9797c23566170dd5f
mpotech-segovia <carlosegovia_mktg@mpotech.net>
2023-04-06 18:00:56 +0800

    pick add ons

--- components/add-ons.js
+++ components/add-ons.js
@@ -1,13 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddOns = () => {
+  const [addOns, setAddOns] = useState([]);
+
+  function handleChange(event) {
+    let value = event.target.value;
+    let name = event.target.name;
+    let isChecked = event.target.checked;
+    var updateAddOns = [...addOns];
+
+    if (isChecked) {
+      updateAddOns = [
+        ...addOns,
+        {
+          name: name,
+        },
+      ];
+    } else {
+      updateAddOns.splice(addOns.indexOf(name), 1);
+    }
+
+    setAddOns(updateAddOns);
+  }
+
   return (
     <div className="add-ons card">
       <div className="card-header">
         <h1>Pick add-ons</h1>
         <p>Add-ons help enhance your gaming experience.</p>
       </div>
-      <div className="card-content"></div>
+      <div className="card-content">
+        <div className="add-ons__wrapper">
+          <div className="add-ons__wrapper-item">
+            <input
+              type="checkbox"
+              value="1"
+              name="Online Service"
+              onChange={handleChange}
+            />
+            <div className="checkbox-content">
+              <div className="item-checkbox"></div>
+              <div className="item-context">
+                <p>Online Service</p>
+                <p>Access to multiplayer games</p>
+              </div>
+              <div className="item-price">+$1/mo</div>
+            </div>
+          </div>
+        </div>
+        <div className="add-ons__wrapper">
+          <div className="add-ons__wrapper-item">
+            <input
+              type="checkbox"
+              value="2"
+              name="Larger Storage"
+              onChange={handleChange}
+            />
+            <div className="checkbox-content">
+              <div className="item-checkbox"></div>
+              <div className="item-context">
+                <p>Larger Storage</p>
+                <p>Extra 1TB of cloud save</p>
+              </div>
+              <div className="item-price">+$2/mo</div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="card-bottom">
         <button className="btn btn-back">Go Back</button>
         <button className="btn btn-next">Next Step</button>
